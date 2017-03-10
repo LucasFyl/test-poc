1b893276e493ac13d5bcd2da9d8858b1ae9da320
David Cingala <david.cingala@gmail.com>
2017-03-10 16:12:26 +0100

    Add inline documentation on stories

--- stories/index.js
+++ stories/index.js
@@ -3,7 +3,7 @@ import {storiesOf, action, linkTo} from '@kadira/storybook';
 import TemplateComponent from '../src';
 
 storiesOf('Template', module)
-    .addWithInfo('active', `Component description`,  () => <TemplateComponent active={true}/>)
-    .addWithInfo('not active', `Component description`,  () => <TemplateComponent active={false}/>);
+    .addWithInfo('active', `Component description`,  () => <TemplateComponent active={true}/>, {inline: true})
+    .addWithInfo('not active', `Component description`,  () => <TemplateComponent active={false}/>, {inline: true});
 
 
