210b124964ab0c87a7de20548adeb7687ab06449
Guillaume Morin <guillaume.morin@adotmob.com>
2017-03-21 10:45:31 +0100

    Update testing to coverage 100%

--- index.test.js
+++ index.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import StyledTemplate from './src';
 import renderer from 'react-test-renderer';
 
-test('Render Template component', () => {
+test('Render Template component active', () => {
     const component = renderer.create(
-        <StyledTemplate></StyledTemplate>
+        <StyledTemplate active={true}></StyledTemplate>
     );
-    let tree = component.toJSON();
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+test('Render Template component not active', () => {
+    const component = renderer.create(
+        <StyledTemplate active={false}></StyledTemplate>
+    );
+    const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
