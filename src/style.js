a200e78a8f68314a3774f3ff20348c439a9e53f5
David Cingala <david.cingala@gmail.com>
2017-04-25 11:30:48 +0200

    Add React component version of template + change style exporting

--- src/style.js
+++ src/style.js
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'aphrodite';
 
-const styles = StyleSheet.create({
+export default StyleSheet.create({
     default: {
         width: 100,
         height: 100
@@ -22,5 +22,3 @@ const styles = StyleSheet.create({
         }
     }
 });
-
-export {styles};
